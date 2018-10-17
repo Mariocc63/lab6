3b74e17a935b6eb6695d7bcefca8b2cc1529325c
Mario Calderón <mariocldrn@gmail.com>
2018-10-16 23:50:08 -0600

    cambio

--- app/controllers/anime.controller.js
+++ app/controllers/anime.controller.js
@@ -24,3 +24,65 @@ exports.update = (req, res) => {
 exports.delete = (req, res) => {
 
 };
+
+// Create and Save a new anime
+exports.create = (req, res) => {
+    // Validate request
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Anime content can not be empty"
+        });
+    }
+
+    // Create a Anime
+    const anime = new Anime({
+        nombre: req.body.title || "Untitled Note", 
+        genero: req.body.content,
+        temporadas: req.body.temporadas
+    });
+
+    // Save Anime in the database
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        });
+    });
+};
+
+// Retrieve and return all animes from the database.
+exports.findAll = (req, res) => {
+    Note.find()
+    .then(animes => {
+        res.send(animes);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving animes."
+        });
+    });
+};
+
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+    Note.findById(req.params.noteId)
+    .then(note => {
+        if(!note) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });            
+        }
+        res.send(note);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving note with id " + req.params.noteId
+        });
+    });
+};
