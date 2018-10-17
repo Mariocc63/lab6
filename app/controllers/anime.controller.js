69c00cbe9b94e33a822811911bb5ee59b1615541
Mario Calderón <mariocldrn@gmail.com>
2018-10-16 23:46:46 -0600

    cambio

---
+++ app/controllers/anime.controller.js
@@ -0,0 +1,26 @@
+const Anime = require('../models/anime.model.js');
+
+// Create and Save a new anime
+exports.create = (req, res) => {
+
+};
+
+// Retrieve and return all animes from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single anime with a animeId
+exports.findOne = (req, res) => {
+
+};
+
+// Update a anime identified by the animeId in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a anime with the specified animeId in the request
+exports.delete = (req, res) => {
+
+};
