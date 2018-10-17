146ab1397f0806d3d3ca5f79565eed4e4720c42e
Mario Calderón <mariocldrn@gmail.com>
2018-10-16 23:57:32 -0600

    cambio

--- app/controllers/anime.controller.js
+++ app/controllers/anime.controller.js
@@ -36,7 +36,7 @@ exports.create = (req, res) => {
 
     // Create a Anime
     const anime = new Anime({
-        nombre: req.body.title || "Untitled Note", 
+        nombre: req.body.title || "Untitled Anime", 
         genero: req.body.content,
         temporadas: req.body.temporadas
     });
@@ -47,7 +47,7 @@ exports.create = (req, res) => {
         res.send(data);
     }).catch(err => {
         res.status(500).send({
-            message: err.message || "Some error occurred while creating the Note."
+            message: err.message || "Some error occurred while creating the Anime."
         });
     });
 };
@@ -65,24 +65,80 @@ exports.findAll = (req, res) => {
 };
 
 
-// Find a single note with a noteId
+// Find a single anime with a animeId
 exports.findOne = (req, res) => {
-    Note.findById(req.params.noteId)
+    Anime.findById(req.params.animeId)
+    .then(anime => {
+        if(!anime) {
+            return res.status(404).send({
+                message: "Anime not found with id " + req.params.animeId
+            });            
+        }
+        res.send(anime);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Anime not found with id " + req.params.animeId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving anime with id " + req.params.animeId
+        });
+    });
+};
+
+// Update a anime identified by the animeId in the request
+exports.update = (req, res) => {
+    // Validate Request
+    if(!req.body.nombre) {
+        return res.status(400).send({
+            message: "Anime name can not be empty"
+        });
+    }
+
+    // Find anime and update it with the request body
+    Anime.findByIdAndUpdate(req.params.animeId, {
+        nombre: req.body.nombre || "Untitled Anime",
+        genero: req.body.genero,
+        temporadas: req.body.temporadas
+    }, {new: true})
+    .then(anime => {
+        if(!anime) {
+            return res.status(404).send({
+                message: "Anime not found with id " + req.params.animeId
+            });
+        }
+        res.send(anime);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Anime not found with id " + req.params.animeId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating anime with id " + req.params.animeId
+        });
+    });
+};
+
+// Delete a anime with the specified animeId in the request
+exports.delete = (req, res) => {
+    Anime.findByIdAndRemove(req.params.noteId)
     .then(note => {
         if(!note) {
             return res.status(404).send({
                 message: "Note not found with id " + req.params.noteId
-            });            
+            });
         }
-        res.send(note);
+        res.send({message: "Note deleted successfully!"});
     }).catch(err => {
-        if(err.kind === 'ObjectId') {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
             return res.status(404).send({
                 message: "Note not found with id " + req.params.noteId
             });                
         }
         return res.status(500).send({
-            message: "Error retrieving note with id " + req.params.noteId
+            message: "Could not delete note with id " + req.params.noteId
         });
     });
 };
