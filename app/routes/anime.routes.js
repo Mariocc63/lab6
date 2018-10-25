09aa8ffc9536e8c21de80772d415d8dd6766ab59
Mariocc63 <mariocldrn@gmail.com>
2018-10-24 18:54:27 -0600

    cambio

--- app/routes/anime.routes.js
+++ app/routes/anime.routes.js
@@ -1,18 +1,13 @@
 module.exports = (app) => {
     const animes = require('../controllers/anime.controller.js');
 
-    // Create a new anime
     app.post('/animes', animes.create);
-
-    // Retrieve all animes
+    
     app.get('/animes', animes.findAll);
-
-    // Retrieve a single anime with animeId
+    
     app.get('/animes/:animeId', animes.findOne);
 
-    // Update a anime with animeId
     app.put('/animes/:animeId', animes.update);
-
-    // Delete a anime with animeId
+    
     app.delete('/animes/:animeId', animes.delete);
 }
