78d26e79e5c819e06d4e16899f57fa775974dce6
Mariocc63 <mariocldrn@gmail.com>
2018-10-24 18:48:14 -0600

    cambio

--- server.js
+++ server.js
@@ -1,46 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// create express app
 const app = express();
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-// Configuring the database
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// Connecting to the database
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
-    console.log("Successfully connected to the database");    
+    console.log("Conexión a la base de datos exitosa");    
 }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+    console.log('No se puede conectar a la base de datos', err);
     process.exit();
 });
 
-
-// define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "CRUD pagina de animes"});
 });
 
-// ........
-
-// Require Animes routes
+// Animes routes
 require('./app/routes/anime.routes.js')(app);
 
-// ........
-
-
-// listen for requests
 app.listen(5000, () => {
-    console.log("Server is listening on port 5000");
+    console.log("Server listening en el puerto 5000");
 });
